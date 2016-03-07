ca3ca4502691584d243a66df07e9005922120c85
Peter Barr <barrpet@gmail.com>
2016-03-07 10:30:07 -0500

    Math Updates

--- src/pbcpp/src/Math/Math.hpp
+++ src/pbcpp/src/Math/Math.hpp
@@ -1,22 +1,20 @@
 #ifndef PBCPP_MATH_HPP
 #define PBCPP_MATH_HPP
 
+#include <type_traits>
+#include <stdexcept>
+
 /*!
  *  \addtogroup Math
  *  @{
  */
 namespace pbcpp { namespace Math {
 
-//! Templated mathematical functions
-
-/*! \fn int_type gcd(int_type u, int_type v)
- * \brief Return the greatest common divisor of two integers
- * \param u an integer (preferably unsigned)
- * \param v an integer (preferably unsigned)
- * \return \a gcd returns the greatest common divisor if two input integers
- */
-template <class int_type>
-int_type gcd(int_type u, int_type v) {
+template <class T>
+T unchecked_gcd(T u, T v)
+{
+  if(u < 0) { u *= -1; }
+  if(v < 0) { v *= -1; }
   int shift;
   if(u == 0) { return v; }
   if(v == 0) { return u; }
@@ -30,14 +28,30 @@ int_type gcd(int_type u, int_type v) {
     while((v & 1) == 0)
       v >>= 1;
     if(u > v) {
-      int_type t = v;
+      T t = v;
       v = u;
       u = t;
     }
     v = v - u;
-  }while(v != 0);
+  } while(v != 0);
   return u << shift;
 }
+
+/*! \fn int_type gcd(int_type u, int_type v)
+ * \brief Return the greatest common divisor of two integers
+ * \param u an integer (preferably unsigned)
+ * \param v an integer (preferably unsigned)
+ * \return \a gcd returns the greatest common divisor if two input integers
+ */
+template <typename _Ta = unsigned int,
+          typename _Tb = _Ta,
+          typename _Tr = typename std::common_type<_Ta, _Ta>::type>
+_Tr gcd(const _Ta& u, const _Tb& v) {
+  static_assert(std::is_integral<_Ta>::value, "gcd: arguments must be integral");
+  static_assert(std::is_integral<_Tb>::value, "gcd: arguments must be integral");
+  return unchecked_gcd(static_cast<_Tr>(u), static_cast<_Tr>(v));
+}
+
 
 template <class int_type, class ret_type = unsigned long long>
 ret_type mul_mod(int_type a, int_type b, int_type mod)
@@ -68,27 +82,28 @@ ret_type mod_exp(int_type base, int_type exp, int_type mod)
   return (ret_type)(x % mod);
 }
 
-/*! \fn bool is_prime_miller_rabin(int_type n)
+/*! \fn bool isPrimeMillerRabin(IntType n)
  * \brief Returns whether or not a number is probably prime
  * \param n The integer to test for primality
  * \param iters The number of iterations to run the test for
  * \return Technically returns either false or probably true
  */
-template <class int_type>
-bool is_prime_miller_rabin(int_type n, unsigned iters = 25)
+template <class _T = unsigned int>
+bool isPrimeMillerRabin(_T n, unsigned int iters = 25)
 {
+  if(n < 0) { throw std::runtime_error("isPrimeMillerRabin: argument must be greater than 0"); }
   if(n < 2) { return false; }
   if(n == 2) { return true; }
   if((n & 1) == 0) { return false; }
   
-  int_type s = n - 1;
+  _T s = n - 1;
   while((s & 1) == 0)
     s >>= 1;
   
-  for(unsigned i = 0; i < iters; i++) {
-    int_type a = rand() % (n - 1) + 1;
-    int_type temp = s;
-    int_type mod = mod_exp(a, temp, n);
+  for(unsigned int i = 0; i < iters; i++) {
+    _T a = rand() % (n - 1) + 1;
+    _T temp = s;
+    _T mod = mod_exp(a, temp, n);
     while(temp != n - 1 && mod != 1 && mod != n - 1)
     {
       mod = mul_mod(mod, mod, n);
--- test/test_math.cpp
+++ test/test_math.cpp
@@ -9,3 +9,50 @@ TEST_CASE("math constants", "[math][constants]")
   REQUIRE( Constants<float>::pi  == Approx( 3.1415926535 ).epsilon( 0.00001 ) );
   REQUIRE( Constants<>::pi  == Approx( 3.1415926535 ).epsilon( 0.00001 ) );
 }
+
+TEST_CASE("math gcd", "[math][gcd]")
+{
+  using namespace pbcpp::Math;
+  SECTION("standard")
+  {
+    REQUIRE( gcd(30, 12) == 6 );
+    REQUIRE( gcd(13452, 864) == 12 );
+    REQUIRE( gcd(543234, 432143) == gcd(432143, 543234) );
+  }
+  
+  SECTION("zero, negatives, equal")
+  {
+    REQUIRE( gcd(14, 0) == 14 );
+    REQUIRE( gcd(0, 111) == 111 );
+    REQUIRE( gcd(756, 756) == 756 );
+    REQUIRE( gcd(-3, 9) == 3);
+    REQUIRE( gcd(9, -3) == 3 );
+    REQUIRE( gcd(-543234, -432143) == gcd(432143, 543234) );
+    REQUIRE( gcd(-543234, 432143) == gcd(432143, -543234) );
+    REQUIRE( gcd(-543234, 432143) == gcd(-432143, 543234) );
+  }
+  
+  SECTION("types")
+  {
+    int a = 30;
+    unsigned b = 12;
+    unsigned char ab = 6;
+    short c = 110;
+    unsigned long long d = 33;
+    long long cd = 11;
+    REQUIRE( gcd(a, b) == ab );
+    REQUIRE( gcd(a, 0) == a );
+    REQUIRE( gcd(0, a) == a );
+    REQUIRE( gcd(c, d) == cd );
+    REQUIRE( gcd(d, c) == cd );
+    REQUIRE( gcd(0, c) == c );
+    REQUIRE( gcd(c, 0) == c );
+    REQUIRE( gcd(d, 0) == d );
+  }
+}
+
+TEST_CASE("math isPrimeMillerRabin", "[math][isPrimeMillerRabin]")
+{
+  using namespace pbcpp::Math;
+  REQUIRE( isPrimeMillerRabin(512927377) );
+}
